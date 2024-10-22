9537e14b5721c80e7bfeb6d6e4f17e98737fdc89
wrtabb <wrtabb@gmail.com>
2024-10-22 12:07:24 -0700

    testing array manipulation with matplotlib with the goal of being able to place arrays of data into histograms

--- Classification/ConvertROOTFiles.py
+++ Classification/ConvertROOTFiles.py
@@ -1,5 +1,6 @@
 import IPython
 import pandas as pd
+import numpy as np
 import fastparquet
 import uproot
 import matplotlib.pyplot as plt
@@ -15,7 +16,21 @@ def load_root_file(file_name):
     branches = tree.arrays()
     #print(branches['Electron_pT'])
 
-    plt.hist(branches['Electron_pT'])
-    plot.show()
+    nEvents = 1000
+    elePt = np.zeros(shape=nEvents)
+    for i in range(nEvents):
+        event_len = len(branches['Electron_pT'][i])
+        if event_len > 0:
+            for j in range(event_len):
+                elePt[i] += branches['Electron_pT'][i][j]
+            # end loop over elements in each event
+        else:
+            elePt[i]=-100
+        # end if length
+    # end loop over events
 
+    plt.hist(elePt,bins=25)
+    plt.xlabel('electron p_T [GeV]')
+    plt.ylabel('num entries')
+    plt.show()
 load_root_file('../Data/background_selection/ZZ.root')
