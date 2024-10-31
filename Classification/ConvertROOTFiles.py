cf795d6ef11f2ce13a5fb329d1f400641eefc388
wrtabb <wrtabb@gmail.com>
2024-10-31 11:37:35 -0700

    fixed issues with creating dataframes for root file data

--- Classification/ConvertROOTFiles.py
+++ Classification/ConvertROOTFiles.py
@@ -5,12 +5,15 @@ import json
 # A function to convert elements with arrays into separate columns
 def expand_arrays(dataframe,var_name):
     print(f'Expanding array:{var_name} into separate columns')
-    old_var= var_name
-    print(f'Old variable: {old_var}')
-    new_var = pd.DataFrame(dataframe[var_name].tolist(), columns=[var_name+'_1', var_name+'_2'])
-    print(f'New variable: {new_var}')
-    dataframe = dataframe.drop(columns=[var_name])
-    dataframe = pd.concat([dataframe, new_var], axis=1)
+    try:
+        new_columns = pd.DataFrame(dataframe[var_name].tolist(), 
+                                   columns = [var_name+'_1', var_name+'_2'],
+                                   index = dataframe.index)
+        dataframe = dataframe.drop(columns=[var_name])
+        dataframe = pd.concat([dataframe, new_columns], axis=1)
+
+    except ValueError as e:
+        print(f"Error expanding {var_name}: {e}")
     return dataframe
 
 # The function that does all the work of loading a tree from a root file, converting tree into a dataframe, applying cuts, then saving to a pickel file
@@ -26,35 +29,35 @@ def load_root_file():
     # define tree variables to save to a dataframe
     #vars_to_keep = data['variables_to_keep']
     ele_vars = data['electron_variables']
-    vtx_vars = data['vertex_variables']
-    pho_vars = data['photon_variables']
     met_vars = data['met_variables']
     num_vars = data['num_variables']
     vars_to_keep = ele_vars+met_vars+num_vars
 
-    num_vars_to_keep = len(vars_to_keep)
-    print(f"Number of variables: {num_vars_to_keep}")
-    # Define list of root files to load from
+    # Define list of root files to load
     file_list = data['root_files_to_load']
 
     # loop over root files to be loaded
-    for x in range(len(file_list)):
-        file_name = '../Data/background_selection/'
-        file_name += file_list[x]
-        cat_name = file_list[x]
-        cat_name = cat_name[:-5]
+    for x in file_list:
+        file_name = '../Data/background_selection/'+x
+        cat_name = x[:-5]
 
         print(f"Loading file: {file_name}")
         
         # load tree from root file using uproot
         with uproot.open(file_name) as file:
             tree = file[tree_name]
+            df_tmp1 = tree.arrays(filter_name=vars_to_keep, library="pd")
 
-        # define dataframe using variables listed in json file
-        df_tmp1 = tree.arrays(filter_name=vars_to_keep, library="pd")
+        # filter rows with 2 electrons 
         df_tmp2 = df_tmp1[df_tmp1['Nelectrons']==2]
 
+        num_before_cut = len(df_tmp1)
+        num_after_cut = len(df_tmp2)
+        print(f'There are {num_before_cut} rows before electron cut')
+        print(f'There are {num_after_cut} rows after electron cut')
+
         # Place all array elements into separate columns for each electron
+        df = df_tmp2
         for x in ele_vars:
             df = expand_arrays(df_tmp2,x)
 
@@ -62,7 +65,7 @@ def load_root_file():
         df['category'] = cat_name 
 
         nrows_expanded = len(df)
-        print(f"After running expand_arrays(), dataframe now has {nrows_expanded} rows after applying dielectron condition")
+        print(f"After running expand_arrays(), dataframe now has {nrows_expanded} rows")
 
         # Define name for save file
         save_name = file_name
@@ -72,8 +75,8 @@ def load_root_file():
         # Save dataframe in pickle file
         print(f"Saving root file as a pickle file here: {save_name}")
         df.to_pickle(save_name)
-
         print(df)
+        del df
     # end loop over file_list
 
 # Run function
