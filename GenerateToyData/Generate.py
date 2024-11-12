5d37f592ffa2fa80084fb8761d9e4cc8a3cd4957
wrtabb <wrtabb@gmail.com>
2024-11-12 13:35:29 -0700

    playing with generating data using python

--- GenerateToyData/Generate.py
+++ GenerateToyData/Generate.py
@@ -8,11 +8,7 @@ def gaussian(x):
     sigma = 20
     return 0.3 * np.exp(-(x-mean)**2 / sigma**2)  # Example: a Gaussian-like shape
 
-def leadpt_pdf(x):
-    mu = 30
-    sigma = -5
-    alpha = 1
-    n = 10
+def leadpt_pdf(x,mu,sigma,alpha,n):
     # Calculate the Gaussian core and tails
     A = (n / abs(alpha)) ** n * np.exp(-alpha**2 / 2)
     B = n / abs(alpha) - abs(alpha)
@@ -27,18 +23,21 @@ def leadpt_pdf(x):
     return np.where(core_condition, gauss, tail)
 
 # Generate samples from the desired distribution
-def generate_samples(pdf, x_min,x_max,num_samples):
+def generate_samples(pdf,mu,sigma,alpha,n,x_min,x_max,num_samples):
     samples = []
     while len(samples) < num_samples:
         x = np.random.uniform(x_min, x_max)
-        y = np.random.uniform(0, 1)  # Adjust range based on the maximum of pdf
-        if y < pdf(x):
+        y = np.random.uniform(0, 1)
+        if y < pdf(x,mu,sigma,alpha,n):
             samples.append(x)
     return np.array(samples)
 
 # Generate samples based on the PDF
 num_samples = 10000
-Electron_pT_1 = generate_samples(leadpt_pdf,0,500,num_samples)
-plt.hist(Electron_pT_1,bins=50,edgecolor='black')
-print(len(Electron_pT_1))
+xlow = 10
+xhigh = 500
+#Electron_pT_1 = generate_samples(leadpt_pdf,30,-5,1,10,xlow,xhigh,num_samples)
+Electron_pT_2 = generate_samples(leadpt_pdf,20,-10,1,10,xlow,xhigh,num_samples)
+
+plt.hist(Electron_pT_2,bins=50,edgecolor='black')
 plt.show()
